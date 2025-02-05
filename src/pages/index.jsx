35c0d0c7bbfb59895455504738bda87f5c4c6e28
Abdullah Iqbal <abdullahiqbal@Abdullah-MacBook-Air.local>
2025-02-05 21:43:42 +0500

    Images added.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,7 @@
+
 export default function Home() {
-  return <div>Welcome</div>;
+  return <div className="bg-[#031f39] h-[1020px]">
+    {/* <image src={bottomright} alt="bottomright" /> */}
+    <img className="h-full w-full" src="/images/bottomright.svg" alt="" />
+  </div>;
 }
