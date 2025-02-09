6f9f5690b3cfc254e196857662024ebceaef33b2
Abdullah Iqbal <abdullahiqbal@Abdullah-MacBook-Air.local>
2025-02-09 12:28:21 +0500

    Landing page started.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,20 +2,23 @@ import LoadingToTick from "../Loader/LoadingToTick"
 
 function Main() {
     return (
-        <div className="absolute top-[100px] w-full h-[600px] grid grid-cols-2 z-20">
-            <div className="flex justify-center min-h-screen">
-                <div className="bg-[#eee] p-6 rounded-2xl shadow-lg w-[450px] h-[450px]">
-                    <h2 className="text-xl font-semibold mb-6 text-center">Shorten a long URL</h2>
+        <div className="absolute top-[100px]  w-full grid grid-cols-2 z-20">
+            <div className="flex justify-center ">
+                <div className="bg-[#F8F8F8] flex flex-col p-6 rounded-2xl shadow-lg w-[500px] h-[450px]">
+                    <div className="flex justify-start items-center mb-6 w-[100%]">
+                        <img className="h-8 w-8 mx-3  object-cover" src="/images/favicon-128.png" alt="" />
+                        <h2 className="text-base antialiased font-bold text-xl font-montserrat">Shorten a long URL</h2>
+                    </div>
                     <input
                         type="text"
                         placeholder="Enter long link here"
-                        className="w-full p-4 border rounded-lg mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="w-full font-medium p-4 border font-montserrat rounded-lg mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                     <div className="mb-8">
-                        <label className="block text-sm font-medium mb-2">Customize your link</label>
+                        <label className="block text-lg font-montserrat font-medium mb-2">Customize your link</label>
                         <div className="flex items-center space-x-2">
                             <select
-                                className="p-2 border rounded-lg w-1/3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                className="p-2 border h-14 font-montserrat font-medium rounded-lg w-full  focus:outline-none focus:ring-2 focus:ring-blue-500"
                             >
                                 <option>tinyurl.com</option>
                                 <option>bit.ly</option>
@@ -23,25 +26,25 @@ function Main() {
                             <input
                                 type="text"
                                 placeholder="Enter alias"
-                                className="flex-1 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                className="flex-1 h-14 p-2 border font-montserrat rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                             />
                         </div>
                     </div>
-                    <div className="flex items-center space-x-4 mb-10">
-                        <div>
+                    <div className="ml-9 flex w-[80%] h-[220px] left-20 border-2 items-center justify-between mb-10">
+                        <div className="ml-5">
                             <LoadingToTick />
                         </div>
                         <div>
 
                         </div>
                     </div>
-                    <button className="w-full bg-green-600 text-white py-3 rounded-lg hover:bg-green-700">
+                    <button className="w-full bg-green-600 text-white py-3 font-montserrat rounded-lg hover:bg-green-700">
                         Shorten URL
                     </button>
                 </div>
             </div>
-            <div className="bg-black h-full flex justify-center items-center">
-                <div className="w-[80%] h-[80%] bg-yellow-900 ">
+            <div className=" h-full flex justify-center items-center">
+                <div className="w-[80%] h-[80%]  ">
 
                 </div>
             </div>
