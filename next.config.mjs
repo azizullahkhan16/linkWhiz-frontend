a06230e1dd0a9862ed5f6692d6a4be0695c8bc1c
Abdullah Iqbal <abdullahiqbal@Abdullah-MacBook-Air.local>
2025-02-10 17:43:02 +0500

    Folder structure corrected.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home-page",
+        permanent: true, // This means it's a permanent redirect (301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
